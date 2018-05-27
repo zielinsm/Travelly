50eca0076b314970ec6e1ab965ceef07c72fefba
zielinskimichal <michalzielinski0@outlook.com>
2018-05-27 13:31:17 +0000

    A clean build

--- dest/assets/js/main.js
+++ dest/assets/js/main.js
@@ -1,10 +1,1 @@
-"use strict";
-
-var menu = document.querySelector(".navigation"),
-    toggle = document.querySelector(".toggle");
-
-function toggleToggle() {
-  menu.classList.toggle("navigation-closed");
-}
-
-toggle.addEventListener("click", toggleToggle, false);
+"use strict";var menu=document.querySelector(".navigation"),toggle=document.querySelector(".toggle");function toggleToggle(){menu.classList.toggle("navigation-closed")}toggle.addEventListener("click",toggleToggle,!1);
