b814b6dae2a74d90fb2a66e0748377e742de3e10
zielinskimichal <michalzielinski0@outlook.com>
2018-05-27 13:18:35 +0000

    A clean build

--- src/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".navigation"),
-  toggle = document.querySelector(".toggle");
-
-function toggleToggle() {
-  menu.classList.toggle("navigation-closed");
-}
-
-toggle.addEventListener("click", toggleToggle, false);
